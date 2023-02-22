118ccc82fed79c2887b92adddfe86c4d80118bda
Brendan Berg <brendan@berg.industries>
2023-02-22 17:39:13 -0500

    rework service lambda

--- service/network.js
+++ service/network.js
@@ -3,10 +3,9 @@ const { DynamoDB } = require('@aws-sdk/client-dynamodb');
 const DDB_TABLE_NAME = "DrawbyNumeralNetworkConnector";
 
 exports.handler = async function (event, context, callback) {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-  // return context.logStreamName;
   const request = event.Records[0].cf.request;
   const resource = request.uri.match(/^\/api\.v1\/net\/(\d{1,4})$/);
+
   if (resource) {
     const addr = parseInt(resource[1], 10);
 
@@ -21,44 +20,71 @@ exports.handler = async function (event, context, callback) {
       return;
     }
 
-
     const ddb = new DynamoDB({ apiVersion: '2012-10-08' });
 
     const params = {
       TableName: DDB_TABLE_NAME,
       Key: {
-        'Addr': { N: resource[1] },
+        'Address': { N: resource[1] },
       }
     };
 
     if (request.method === 'GET') {
       // Call DynamoDB to read the item from the table
-      ddb.getItem(params, function (err, data) {
-        if (err) {
-          console.log("Error", err);
-          callback(null, {
-            body: '',
-            bodyEncoding: 'text',
-            status: '500',
-            statusDescription: 'Internal Server Error'
-          });
-        } else {
-          console.log("Success", data.Item);
-          callback(null, {
-            body: data.Item,
-            bodyEncoding: 'text',
-            status: '200',
-            statusDescription: 'OK'
-          });
-        }
-      });
-      //return;
+      try {
+        const data = await ddb.getItem(params);
+
+        callback(null, {
+          body: data.Item?.Value?.N || '0',
+          bodyEncoding: 'text',
+          status: '200',
+          statusDescription: 'OK'
+        });
+
+        console.log({
+          method: 'GET',
+          path: request.uri,
+          address: resource[1],
+          value: data.Item?.Value?.N || '0'
+        });
+      } catch (err) {
+        callback(null, {
+          body: '',
+          bodyEncoding: 'text',
+          status: '500',
+          statusDescription: 'Internal Server Error'
+        });
+
+        console.log({
+          method: 'GET',
+          path: request.uri,
+          address: resource[1],
+          error: err
+        });
+      }
 
     } else if (request.method === 'PUT') {
-      const body = request.body.match(/^(\d+)$/);
+      if (request.body.encoding !== 'base64' || request.body.inputTruncated === true) {
+        callback(null, {
+          body: '',
+          bodyEncoding: 'text',
+          status: 400,
+          statusDescription: 'Bad Request'
+        });
+
+        console.log({
+          method: 'PUT',
+          path: request.uri,
+          address: resource[1],
+          error: 'unable to parse request body'
+        })
+      }
+
+      const body = Buffer.from(request.body.data, 'base64').toString('ascii').trim();
+      const match = body.match(/^(\d+)$/);
 
       // There was no parsable number in the request body
-      if (!body) {
+      if (!match) {
         callback(null, {
           body: '',
           bodyEncoding: 'text',
@@ -68,36 +94,49 @@ exports.handler = async function (event, context, callback) {
         return;
       }
 
-      const value = parseInt(body[1], 10);
+      //const value = parseInt(match[1], 10);
 
       Object.assign(params, {
-        UpdateExpression: "set Value = :val",
+        UpdateExpression: "set #V = :val",
+        ExpressionAttributeNames: {
+          "#V": "Value"
+        },
         ExpressionAttributeValues: {
-            ":val": { N: body[1] },
+          ":val": { N: match[1] },
         }
       });
 
-      ddb.updateItem(params, function(err, data) {
-        if (err) {
-          console.log(err);
-          callback(null, {
-            body: '',
-            bodyEncoding: 'text',
-            status: '500',
-            statusDescription: 'Internal Server Error'
-          });
-        } else {
-          console.log(data);
-          callback(null, {
-            body: value.toString(),
-            bodyEncoding: 'text',
-            status: '200',
-            statusDescription: 'OK'
-          });    
-        }
-      });
+      try {
+        const data = await ddb.updateItem(params);
 
-      //return;
+        callback(null, {
+          body: match[1],
+          bodyEncoding: 'text',
+          status: '200',
+          statusDescription: 'OK'
+        });
+
+        console.log({
+          method: 'PUT',
+          path: request.uri,
+          address: resource[1],
+          value: match[1]
+        });
+      } catch (err) {
+        callback(null, {
+          body: '',
+          bodyEncoding: 'text',
+          status: '500',
+          statusDescription: 'Internal Server Error'
+        });
+
+        console.log({
+          method: 'PUT',
+          path: request.uri,
+          address: resource[1],
+          error: err
+        });
+      }
     } else {
 
       // Only GET and PUT requests are allowed
@@ -107,8 +146,6 @@ exports.handler = async function (event, context, callback) {
         status: '405',
         statusDescription: 'Method Not Allowed'
       });
-      return;
-
     }
   } else {
 
@@ -116,12 +153,6 @@ exports.handler = async function (event, context, callback) {
     const response = {
       body: '',
       bodyEncoding: 'text',
-      // headers: {
-      //     'header name in lowercase': [{
-      //         key: 'header name in standard case',
-      //         value: 'header value'
-      //      }],
-      // },
       status: '404',
       statusDescription: 'Not Found'
     };
